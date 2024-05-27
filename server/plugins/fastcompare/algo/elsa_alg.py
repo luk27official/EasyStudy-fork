b15ac325c949c9df628dd22518f43f16116c0fb4
Lukas Polak <admin@lukaspolak.cz>
2024-05-27 20:29:51 +0200

    Add diversity

--- server/plugins/fastcompare/algo/elsa_alg.py
+++ server/plugins/fastcompare/algo/elsa_alg.py
@@ -21,9 +21,14 @@ class ELSAWrapper(AlgorithmBase, ABC):
         self._items_count = self._rating_matrix.shape[1]
         self._weights = None
         self._factors = kwargs.get("factors", 256)
-        self._device = torch.device(kwargs.get("device", "cuda"))
+        self._device = torch.device(kwargs.get("device", "cpu"))
+        if kwargs.get("device") == "cuda" and not torch.cuda.is_available():
+            print("CUDA is not available, using CPU instead.")
+            self._device = torch.device("cpu")
         self._num_epochs = kwargs.get("num_epochs", 5)
         self._batch_size = kwargs.get("batch_size", 128)
+        self._postprocess = kwargs.get("postprocess", True)
+        self._diversity_threshold = kwargs.get("diversity_threshold", 0.9)
 
         self.model = ELSA(n_items=self._items_count, device=self._device, n_dims=self._factors)
         self.A = None  # for storing the item embeddings
@@ -53,6 +58,9 @@ class ELSAWrapper(AlgorithmBase, ABC):
         predictions[:, filter_out_items] = -np.inf  # Filter out specified items
         top_k_items = np.argsort(predictions, axis=1)[:, -k:][:, ::-1]  # Get top-k items
 
+        if self._postprocess:
+            top_k_items = self._apply_diversity(top_k_items)
+
         return top_k_items.flatten()
 
     def _get_sparse_interaction_matrix(self, rating_matrix):
@@ -71,6 +79,41 @@ class ELSAWrapper(AlgorithmBase, ABC):
         user_interactions[0, selected_items] = 1
         return user_interactions
 
+    def _apply_diversity(self, top_k_items):
+        """
+        Apply diversity to the recommended items.
+
+        Parameters:
+        - top_k_items: list of top-k recommended item indices.
+
+        Returns:
+        - diversified_items: list of diversified top-k recommended item indices.
+        """
+        diversified_items = []
+        for item in top_k_items[0]:
+            if not self._is_too_similar(item, diversified_items):
+                diversified_items.append(item)
+                if len(diversified_items) >= len(top_k_items[0]):
+                    break
+        return np.array(diversified_items).reshape(1, -1)
+
+    def _is_too_similar(self, item, diversified_items):
+        """
+        Check if the item is too similar to any of the already selected items.
+
+        Parameters:
+        - item: item index to check.
+        - diversified_items: list of already selected diversified items.
+
+        Returns:
+        - is_similar: boolean indicating if the item is too similar.
+        """
+        for div_item in diversified_items:
+            similarity = np.dot(self.A[item], self.A[div_item])
+            if similarity > self._diversity_threshold:
+                return True
+        return False
+
     @classmethod
     def name(cls):
         return "ELSA"
@@ -78,10 +121,16 @@ class ELSAWrapper(AlgorithmBase, ABC):
     @classmethod
     def parameters(cls):
         return [
-            Parameter("factors", ParameterType.INT, 256),
+            Parameter("factors", ParameterType.INT, 256, help="Number of latent factors for the model."),
+            Parameter("device", ParameterType.STRING, "cpu", help="Device to run the model on ('cuda' or 'cpu')."),
+            # Parameter("device", ParameterType.OPTIONS, "cpu", "cuda"), # TODO: try this, but I don't know the options format
+            Parameter("num_epochs", ParameterType.INT, 5, help="Number of epochs to train the model."),
+            Parameter("batch_size", ParameterType.INT, 128, help="Batch size for training."),
+            Parameter("postprocess", ParameterType.BOOL, True, help="Enable or disable postprocessing for diversity."),
             Parameter(
-                "device", ParameterType.STRING, "cpu"
-            ),  # "cuda" or "cpu" (for some reason, "cuda" is not working in this case for me)
-            Parameter("num_epochs", ParameterType.INT, 5),
-            Parameter("batch_size", ParameterType.INT, 128),
+                "diversity_threshold",
+                ParameterType.FLOAT,
+                0.9,
+                help="Threshold for diversity in recommendations. Higher values mean less diversity.",
+            ),
         ]
