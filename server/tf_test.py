74a4c61c0bbfef3c1b2e7a7ad22a0c762082e39a
Lukas Polak <admin@lukaspolak.cz>
2024-05-27 13:48:11 +0200

    TensorFlow GPU test

--- server/tf_test.py
+++ server/tf_test.py
@@ -5,3 +5,10 @@ if __name__ == "__main__":
 
     print(tf.__version__)
     print(tf.config.list_physical_devices("GPU"))
+
+    with tf.device("/gpu:0"):
+        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name="a")
+        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name="b")
+        c = tf.matmul(a, b)
+
+        print(c)
